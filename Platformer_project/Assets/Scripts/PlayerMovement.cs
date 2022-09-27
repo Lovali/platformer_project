60878dd954fc4b30428cc62741dc747d35cde2c0
Shih Emilie <emilie.shih@telecom-sudparis.eu>
2022-09-27 15:52:40 +0200

    Added horizontal movement

--- Platformer_project/Assets/Scripts/PlayerMovement.cs
+++ Platformer_project/Assets/Scripts/PlayerMovement.cs
@@ -1,19 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 moveVal;
+    public float moveSpeed;
+    void OnMove(InputValue value)
     {
-        
+        moveVal = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(moveVal.x, 0, 0) * moveSpeed * Time.deltaTime);
     }
 }
