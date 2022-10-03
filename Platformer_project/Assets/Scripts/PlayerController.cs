f95c604bc55adc7e9b5bbfbd3b6a3ce0c9143f69
Shih Emilie <emilie.shih@telecom-sudparis.eu>
2022-10-03 21:22:19 +0200

    Added dash and sprint system

--- Platformer_project/Assets/Scripts/PlayerController.cs
+++ Platformer_project/Assets/Scripts/PlayerController.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private bool isOnTheGround;
-    // Start is called before the first frame update
+    
     void Start()
     {
         isOnTheGround = false;
--- Platformer_project/Assets/Scripts/PlayerMovement.cs
+++ Platformer_project/Assets/Scripts/PlayerMovement.cs
@@ -11,9 +11,15 @@ public class PlayerMovement : MonoBehaviour
     private bool againstRightWall = false;
     private bool canDoubleJump = false;
 
-    public float jumpForce = 10;
-    public float gravity = -5f;
+    [SerializeField] private float jumpForce = 10;
+    [SerializeField] private float gravity = -5f;
     private float velocity;
+
+    [SerializeField] private float dashForce = 3;
+    private bool dashPressed;
+
+    [SerializeField] private float sprintMultiplier = 3;
+    private bool sprintPressed;
 
     PlayerController playerController;
     private void Awake()
@@ -30,8 +36,46 @@ public class PlayerMovement : MonoBehaviour
         jumpPressed = value.isPressed;
     }
 
+    void OnDash(InputValue value)
+    {
+        dashPressed = value.isPressed;
+    }
+
+    void OnSprint(InputValue value)
+    {
+        sprintPressed = value.isPressed;
+    }
+
     void Update()
     {
+        if (dashPressed)
+        {
+            Vector3 pre_pos = transform.position;
+            Vector3 dash = new Vector3(moveVal.x * dashForce, 0, 0) * moveSpeed * Time.deltaTime;
+            RaycastHit2D hit = Physics2D.Linecast(pre_pos, pre_pos + dash, LayerMask.GetMask("Collision"));
+
+            if (hit.collider)
+            {
+                transform.Translate(new Vector3(pre_pos.x - hit.collider.transform.position.x, 0, 0) * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(dash);
+            }
+        }
+
+        if ((againstLeftWall && moveVal.x > 0) || (againstRightWall && moveVal.x < 0) || (!againstRightWall && !againstLeftWall))
+        {
+            if(sprintPressed)
+            {
+                transform.Translate(new Vector3(moveVal.x * sprintMultiplier, 0, 0) * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                transform.Translate(new Vector3(moveVal.x, 0, 0) * moveSpeed * Time.deltaTime);
+            }            
+        }
+
         /*transform.Translate(new Vector3(moveVal.x, 0, 0) * moveSpeed * Time.deltaTime);
 
         if (againstLeftWall)
