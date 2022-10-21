9f2cc6accd9bc8c5fc2be98c58c4bff6d813ffce
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-10-21 14:11:03 +0200

    Feedback when player lose health because of damage platform implemented

--- Platformer_project/Assets/Scripts/Damage.cs
+++ Platformer_project/Assets/Scripts/Damage.cs
@@ -2,14 +2,34 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
-    [SerializeField] PlayerHealth playerHealth;
+    [SerializeField] GameObject player;
+    private PlayerHealth playerHealth;
     [SerializeField] int damage = 1;
+
+    private void Awake()
+    {
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             playerHealth.TakeDamage(damage);
+            ChangeColor();
+            Invoke(nameof(ChangeColor), 0.25f);
+        }
+    }
+
+    private void ChangeColor()
+    {
+        if (player.GetComponent<SpriteRenderer>().color == Color.red)
+        {
+            player.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        else
+        {
+            player.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 }
